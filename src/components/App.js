d037db24303db56ce242b45dd466780f87fd648a
Adreena Fulcher <hurlocene@gmail.com>
2021-06-16 09:51:14 -0400

    search isn't working

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([])
+  const [term, setTerm] = useState("")
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+    .then(res => res.json())
+    .then(data => setListings(data))
+  }, [])
+
+  // function handleSort() {
+  //   setSort
+  // }
+
+  function deleteListing(listingId){
+   let filteredArray = listings.filter(listing => listing.id !== listingId)
+
+   setListings(filteredArray)
+  }
+  
+  let filteredListings = listings.filter(listing => listing.description.toLowerCase().includes(term.toLowerCase()))
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header term={term} setTerm={setTerm}/>
+      <ListingsContainer listings={listings} deleteListing={deleteListing}/>
     </div>
   );
 }
